523a8a98a222de97d784a959a4fd83e404645aa1
emerya2002 <95879915+emerya2002@users.noreply.github.com>
2024-02-19 14:01:05 -0500

    Add files via upload

---
+++ Decorator.java
@@ -0,0 +1,35 @@
+package assignment02;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public abstract class Decorator extends Component {
+	private Component delegate;
+	
+	public Decorator(Component delegateIn) {
+		delegate = delegateIn;
+	}
+
+	public abstract Set<Character> getConsonantsInWord();
+	public abstract Set<Character> getVowelsInWord();	
+	
+	@Override
+	public Set<Character> getConsonants() {
+		// TODO Auto-generated method stub
+		Set<Character> consonants = new HashSet<>(delegate.getConsonants());
+		consonants.addAll(getConsonantsInWord());
+		return consonants;
+	}
+
+	@Override
+	public Set<Set<Character>> getVowels() {
+		// TODO Auto-generated method stub
+		Set<Set<Character>>  vowels = new HashSet<>(delegate.getVowels());
+		vowels.add(getVowelsInWord());
+		return vowels;
+	}
+
+	@Override
+	public void print() {
+		delegate.print();
+	}}
